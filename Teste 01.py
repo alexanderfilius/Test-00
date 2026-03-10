3a07a40fdec256f709e3e3e46121cd6ec99b200f
Alexandre Nativitatis Filius <alexanderfilius.dev@gmail.com>
2026-03-10 16:43:00 -0300

    Test 01
    if
    elif
    else

---
+++ Teste 01.py
@@ -0,0 +1,7 @@
+nome = input('Nome: ')
+if nome == 'José':
+    print('Cabra da peste!!!')
+elif nome == 'Maria':
+    print(' Jo 2:4: "Jesus lhe disse: Mulher, que tenho eu para fazer contigo? Ainda não é chegada a minha hora." ')
+else:
+     print('Faça elevar o cosmo no seu coração!!!')
